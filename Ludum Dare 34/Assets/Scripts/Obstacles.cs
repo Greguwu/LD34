fd497780484a40bbb05286a342afebe1799d21f3
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-12-12 20:12:55 +0100

    Added obstacles

---
+++ Ludum Dare 34/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacles : MonoBehaviour {
+
+    public float downScalingFactor;
+    private bool hasCollidedOnce;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (!hasCollidedOnce)
+        {
+            HurtPlayer(other);
+        }
+    }
+
+    void HurtPlayer(Collision2D other)
+    {
+        Debug.Log("wesh");
+        hasCollidedOnce = true;
+        other.gameObject.GetComponent<Player>().scalePlayer(-downScalingFactor);
+    }
+}
