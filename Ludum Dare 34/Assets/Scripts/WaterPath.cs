abad9104e18379604771ef90a7345afe29b8699e
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-12-13 17:47:03 +0100

    Y'a un début de path qui marche

---
+++ Ludum Dare 34/Assets/Scripts/WaterPath.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterPath : MonoBehaviour {
+
+    private BezierSpline thisSpline;
+    public float pathDuration;
+
+	// Use this for initialization
+	void Start () {
+        thisSpline = GetComponent<BezierSpline>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator WaitEnd(Collider2D other)
+    {
+        Debug.Log("wesh");
+        yield return new WaitForSeconds(pathDuration);
+        other.GetComponent<SplineWalker>().enabled = false;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() == true)
+        {
+            other.GetComponent<SplineWalker>().duration = pathDuration;
+            other.GetComponent<SplineWalker>().spline = thisSpline;
+            other.GetComponent<SplineWalker>().enabled = true;
+            StartCoroutine(WaitEnd(other));
+        }
+    }
+}
