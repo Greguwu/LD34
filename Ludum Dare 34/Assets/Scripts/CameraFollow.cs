bfe4de79881ad004899da262ade82951f8f7433a
Grégoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2015-12-13 12:41:32 +0100

    J'ai pas tout cassé, ça a marché

--- Ludum Dare 34/Assets/Scripts/CameraFollow.cs
+++ Ludum Dare 34/Assets/Scripts/CameraFollow.cs
@@ -6,31 +6,31 @@ public class CameraFollow : MonoBehaviour {
     [HideInInspector]
     public Player playerObject;
     private SpriteRenderer background;
+    private GameObject secondCamera;
     public float playerEnd;
     private float backParallax;
     private float playerStartY;
     private float backStartY;
-    private TrailRenderer tr;
     private GameObject normalMap;
     public float normalMapScrollSpeed;
 
     void Awake()
     {
+        secondCamera = GameObject.FindGameObjectWithTag("Second Camera").transform.gameObject;
         playerObject = GameObject.FindGameObjectWithTag("Player").transform.parent.GetComponent<Player>();
         background = GetComponentInChildren<SpriteRenderer>();
         normalMap = GetComponentInChildren<MeshRenderer>().transform.gameObject;
         backStartY = background.transform.localPosition.y;
         playerStartY = playerObject.transform.localPosition.y;
-        tr = playerObject.GetComponentInChildren<TrailRenderer>();
     }
 
     void Update()
     {
-        transform.position = new Vector3(0, playerObject.transform.position.y -2, -10);
+        transform.position = new Vector3(0, playerObject.transform.position.y - 2, -10);
+        secondCamera.transform.position = new Vector3(secondCamera.transform.position.x, playerObject.transform.position.y - 2, -10);
         backParallax = Maths.Remap(playerObject.transform.localPosition.y, playerStartY, playerEnd, backStartY, backStartY * -1);
         background.transform.localPosition = new Vector3(0, backParallax, 10);
         normalMap.transform.localPosition -= new Vector3(0, normalMapScrollSpeed/100, 0);
-        tr.sortingLayerName = "Character";
     }
 
 }
